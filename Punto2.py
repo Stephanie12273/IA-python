7ff0a3ad87476bcaf4180a7466d7f8eb9770b869
Camilo Garcia <yashircamilo@gmail.com>
2024-03-27 18:00:06 -0500

    Funcionando hasta un punto

--- Punto2.py
+++ Punto2.py
@@ -1,9 +1,29 @@
 import pandas as pd 
-import scipy as sp
-from sklearn import preprocessing
+import sklearn 
+import matplotlib.pyplot as plt 
 
-DB = pd.read_csv('Archivos\ecommerce_data.csv')
+#importamos modulos 
+from sklearn.decomposition import PCA
+from sklearn.decomposition import IncrementalPCA
 
-x = sp.stats.describe(DB)
-print (x)
+from sklearn.linear_model import LogisticRegression 
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
+if __name__== "__main__":
+    dt= pd.read_csv('./Archivos/telecom_churn_data.csv')
+    #print(dt.head(5))
+    dt_features = dt.drop(['Churn'], axis=1) 
+    dt_target = dt['Churn']
+
+    dt_features = StandardScaler().fit_transform(dt_features)
+    
+    X_train, X_test, y_train, y_test = train_test_split(dt_features, dt_target, test_size=0.3, random_state=42)
+    print(X_train.shape)
+    print(y_train.shape)
+
+    #n_components = min(n_muestras, n_features)
+    #pca = PCA(n_components=)
+
+    
